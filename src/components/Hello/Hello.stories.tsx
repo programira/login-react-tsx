77e72daff5d5c317da31d449a634a0786eae3310
Mirjana Milosevic <mirjana.milosevic@mapp.com>
2020-05-24 23:12:53 +0200

    Adding Login form

--- src/components/Hello/Hello.stories.tsx
+++ src/components/Hello/Hello.stories.tsx
@@ -3,4 +3,4 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Hello from './Hello';
 
-storiesOf('Hello', module).add('default', () => <Hello title="test"/>);
+storiesOf('Hello', module).add('default', () => <Hello />);
